7f5542c80e97eb5c33871a84f88ae46535d5fa0e
SeaHOH <Seahoh@Gmail.com>
2020-10-18 23:21:45 +0800

    Lazy load polyfill, default from unpkg, no longer minified, automated hashing

--- release.cmd
+++ release.cmd
@@ -4,6 +4,10 @@ set VER=1.0.3
 sed -i -E "s/version>.+?</version>%VER%</" install.rdf
 sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/github-wc-polyfill-.+?\.xpi/download\/%VER%\/github-wc-polyfill-%VER%\.xpi/" update.xml
 
+set CONTENT=content
+if not exist %CONTENT% md %CONTENT%
+node update_polyfill.js
+
 set XPI=github-wc-polyfill-%VER%.xpi
 if exist %XPI% del %XPI%
 zip -r9q %XPI% bootstrap.js icon.png install.rdf
--- update_polyfill.js
+++ update_polyfill.js
@@ -7,27 +7,11 @@ var crypto = require('crypto');
 var version = "2.4.4";
 var bundle = "webcomponents-bundle.js";
 var url = "https://unpkg.com/@webcomponents/webcomponentsjs@" + version + "/" + bundle;
-var bootstrap = "bootstrap.js";
-
-var handle = function() {
-  var bdata = fs.readFileSync(bundle, "utf8");
-  bdata = bdata.replace(/\/\*(\r?\n|\*)[\s\S]+?\*\//g, "");
-  bdata = bdata.replace(/(\r?\n)+\/\/[\s\S]+/, "").trim();
-  fs.writeFileSync(bundle.replace("bundle", "include"), bdata);
-  var hash = crypto.createHash("sha256").update(bdata).digest("base64");
-  var b64 = Buffer.from(bdata).toString("base64");
-  var sdata = fs.readFileSync(bootstrap, "utf8");
-  sdata = sdata.replace(/const sha256.+?;/, "const sha256 = \"'sha256-" + hash + "'\";");
-  sdata = sdata.replace(/const polyfill.+?;/, "const polyfill = atob(\"" + b64 + "\");");
-  fs.writeFileSync(bootstrap, sdata);
-}
+bundle = "content/" + bundle;
 
 try { fs.unlinkSync(bundle) } catch(e) {}
 
 var file = fs.createWriteStream(bundle);
 https.get(url, function(response) {
   response.pipe(file);
-  file.on('finish', function() {
-    file.close(handle);
-  });
 });
