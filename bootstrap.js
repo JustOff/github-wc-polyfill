d1bec1557906e8c9b1fd67364320ad16fb5be733
JustOff <Off.Just.Off@gmail.com>
2020-10-22 21:40:52 +0300

    Support SeaMonkey (experimental)
    Related request: #8.

--- bootstrap.js
+++ bootstrap.js
@@ -13,7 +13,30 @@ var Cc = Components.classes, Ci = Components.interfaces, Cu = Components.utils;
 Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
-var httpResponseObserver = {
+const isSeaMonkey = Services.appinfo.name == "SeaMonkey";
+
+const polyfill = `if (!Element.prototype.toggleAttribute) {
+  Element.prototype.toggleAttribute = function(name, force) {
+    if(force !== void 0) force = !!force 
+    
+    if (this.hasAttribute(name)) {
+      if (force) return true;
+
+      this.removeAttribute(name);
+      return false;
+    }
+    if (force === false) return false;
+      
+    this.setAttribute(name, "");
+    return true;
+  };
+}`;
+
+const sha256 = "'sha256-a1mbTwJSodgsH6xIck4zziyRaF6F6MYFDaWPqucYPeo='";
+
+var cookie;
+
+var httpObserver = {
   observe: function (subject, topic, data) {
     if ((topic == "http-on-examine-response" || topic == "http-on-examine-cached-response") &&
         subject instanceof Ci.nsIHttpChannel &&
@@ -25,6 +48,12 @@ var httpResponseObserver = {
              subject.loadInfo.externalContentPolicyType == Ci.nsIContentPolicy.TYPE_SUBDOCUMENT)) {
           let ctype = subject.getResponseHeader("Content-Type").toLowerCase();
           if (ctype.indexOf("text/html") != -1) {
+            if (isSeaMonkey) {
+              let csp = subject.getResponseHeader("Content-Security-Policy");
+              csp = csp.replace("script-src ", "script-src github.com " + sha256 + " ");
+              csp = csp.replace("default-src 'none'", "default-src github.com/socket-worker.js gist.github.com/socket-worker.js");
+              subject.setResponseHeader("Content-Security-Policy", csp, false);
+            }
             subject.QueryInterface(Ci.nsITraceableChannel);
             let newListener = new tracingListener();
             newListener.originalListener = subject.setNewListener(newListener);
@@ -35,6 +64,15 @@ var httpResponseObserver = {
           subject.setResponseHeader("Content-Security-Policy", csp, false);
         }
       } catch (e) {}
+    } else if (isSeaMonkey && topic == "http-on-modify-request" && subject instanceof Ci.nsIHttpChannel &&
+        (subject.URI.host == "github.com" || subject.URI.host == "gist.github.com")) {
+      try {
+        cookie = subject.getRequestHeader("Cookie");
+      } catch (e) {
+        if (cookie) {
+          subject.setRequestHeader("Cookie", cookie, false);
+        }
+      }
     }
   },
   QueryInterface: XPCOMUtils.generateQI([Ci.nsIObserver, Ci.nsISupportsWeakReference])
@@ -63,6 +101,9 @@ tracingListener.prototype = {
     try {
       data = data.replace(/<script crossorigin="anonymous" defer="defer" (.+?compat\.js.+?)data-src=(.+?)\/script>/,
                           "<script crossorigin=\"anonymous\" $1src=$2/script>");
+      if (isSeaMonkey) {
+        data = data.replace("<head>", "<head><script>" + polyfill + "</script>");
+      }
     } catch (e) {}
     let storageStream = Cc["@mozilla.org/storagestream;1"].createInstance(Ci["nsIStorageStream"]);
     storageStream.init(8192, data.length, null);
@@ -82,16 +123,22 @@ tracingListener.prototype = {
 }
 
 function startup(data, reason) {
-  Services.obs.addObserver(httpResponseObserver, "http-on-examine-response", false);
-  Services.obs.addObserver(httpResponseObserver, "http-on-examine-cached-response", false);
+  Services.obs.addObserver(httpObserver, "http-on-examine-response", false);
+  Services.obs.addObserver(httpObserver, "http-on-examine-cached-response", false);
+  if (isSeaMonkey) {
+    Services.obs.addObserver(httpObserver, "http-on-modify-request", false);
+  }
 }
 
 function shutdown(data, reason) {
   if (reason == APP_SHUTDOWN) {
     return;
   }
-  Services.obs.removeObserver(httpResponseObserver, "http-on-examine-cached-response", false);
-  Services.obs.removeObserver(httpResponseObserver, "http-on-examine-response", false);
+  if (isSeaMonkey) {
+    Services.obs.removeObserver(httpObserver, "http-on-modify-request", false);
+  }
+  Services.obs.removeObserver(httpObserver, "http-on-examine-cached-response", false);
+  Services.obs.removeObserver(httpObserver, "http-on-examine-response", false);
 }
 
 function install() {}
