e9f13a9f6239a32acff380a82a7560c02fed687d
JustOff <Off.Just.Off@gmail.com>
2021-10-24 23:44:17 +0300

    Bump version

--- release.cmd
+++ release.cmd
@@ -1,5 +1,5 @@
 @echo off
-set VER=1.2.5
+set VER=1.2.6
 
 sed -i -E "s/version>.+?</version>%VER%</" install.rdf
 sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/github-wc-polyfill-.+?\.xpi/download\/%VER%\/github-wc-polyfill-%VER%\.xpi/" update.xml
