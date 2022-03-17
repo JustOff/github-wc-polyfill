082dfd7d85645e71c1e35973f2833c3fbaaf76a7
SeaHOH <Seahoh@Gmail.com>
2022-03-18 01:08:54 +0800

    Bump version

--- release.cmd
+++ release.cmd
@@ -1,5 +1,5 @@
 @echo off
-set VER=1.2.16
+set VER=1.2.17
 
 sed -i -E "s/version>.+?</version>%VER%</" install.rdf
 sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/github-wc-polyfill-.+?\.xpi/download\/%VER%\/github-wc-polyfill-%VER%\.xpi/" update.xml
